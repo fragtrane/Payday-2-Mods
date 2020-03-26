a0a982a7852a2a302c26da60186c8a45c52dd51d
fragtrane <47076535+fragtrane@users.noreply.github.com>
2020-03-26 14:39:16 +0100

    Super Duper Skin Swapper v1.3.1

--- Super Duper Skin Swapper/Super Duper Skin Swapper/lua/networkpeer.lua
+++ Super Duper Skin Swapper/Super Duper Skin Swapper/lua/networkpeer.lua
@@ -3,40 +3,32 @@ dofile(ModPath .. "lua/setup.lua")
 --Prevent false-positives in the anti-piracy detection. default_blueprint was removed so legacy_blueprint needs to be checked.
 local orig_NetworkPeer__verify_content = NetworkPeer._verify_content
 function NetworkPeer:_verify_content(item_type, item_id)
-	--Replace dummy in order to validate Akimbo Kobus 90 and Akimbo Judge
-	if item_id == "wpn_fps_smg_p90_b_legend_dummy" then
-		item_id = "wpn_fps_smg_p90_b_legend"
-	elseif item_id == "wpn_fps_pis_judge_b_legend_dummy" then
-		item_id = "wpn_fps_pis_judge_b_legend"
-	elseif item_id == "wpn_fps_pis_judge_g_legend_dummy" then
-		item_id = "wpn_fps_pis_judge_g_legend"
-	end
-	--Original result
+	--Original result. Dummy mods will be flagged as well.
 	local result = orig_NetworkPeer__verify_content(self, item_type, item_id)
 	--If a weapon mod is not okay, check legacy blueprint to see if it is included.
 	--Check both primary and secondary.
-	if not result and tostring(item_type) == "weapon_mods" then
+	if not result and item_type == "weapon_mods" then
+		--If dummy mod, change name to real mod
+		local is_dummy = false
+		local dummy_mods = {
+			"wpn_fps_smg_p90_b_legend_dummy",
+			"wpn_fps_pis_judge_b_legend_dummy",
+			"wpn_fps_pis_judge_g_legend_dummy"
+		}
+		if table.contains(dummy_mods, item_id) then
+			item_id = item_id:sub(1, -7)
+			is_dummy = true
+		end
+		--Check if mod is in legacy_blueprint
 		local we_good = true
 		local outfit = self:blackmarket_outfit()
 		for item_type, item in pairs(outfit) do
 			if item_type == "primary" or item_type == "secondary" then
-				if table.contains(item.blueprint, item_id) then
+				if table.contains(item.blueprint, item_id..(is_dummy and "_dummy" or "")) then
 					local skin_blueprint = item.cosmetics and tweak_data.blackmarket.weapon_skins[item.cosmetics.id].legacy_blueprint or {}
 					if not table.contains(skin_blueprint, item_id) then
 						we_good = false
-						--Tempfix: if legendary attachment and not on original gun, ignore.
-						--Do this to avoid false positives: if another player has extended their uses_parts table, we will think they are using a legendary part regardless of what they are actually using.
-						if item_id == "wpn_fps_pis_deagle_b_legend" and item.factory_id ~= "wpn_fps_pis_deagle" then
-							we_good = true
-						elseif item_id == "wpn_fps_pis_1911_g_legendary" and item.factory_id ~= "wpn_fps_x_1911" then
-							we_good = true
-						elseif item_id == "wpn_fps_pis_1911_fl_legendary" and item.factory_id ~= "wpn_fps_x_1911" then
-							we_good = true
-						end
-						--Break if invalid mod
-						if not we_good then
-							break
-						end
+						break
 					end
 				end
 			end
--- Super Duper Skin Swapper/Super Duper Skin Swapper/lua/weaponfactorytweakdata.lua
+++ Super Duper Skin Swapper/Super Duper Skin Swapper/lua/weaponfactorytweakdata.lua
@@ -1,7 +1,7 @@
 dofile(ModPath .. "lua/setup.lua")
 
 --Set some adds/forbids to prevent legendary attachment clipping.
---Do not add or delete legendary mods from uses_parts here, can cause sync issues/cheater tags.
+--Do not add or delete legendary mods from uses_parts, can cause sync issues/cheater tags.
 Hooks:PostHook(WeaponFactoryTweakData, "init", "sdss_post_WeaponFactoryTweakData_init", function(self)
 	if not SDSS._settings.sdss_allow_variants then
 		--Change to dummy part to hide
@@ -58,35 +58,14 @@ Hooks:PostHook(WeaponFactoryTweakData, "init", "sdss_post_WeaponFactoryTweakData
 	end	
 end)
 
---Insert legendary parts AFTER bonuses. Otherwise, when peers send index of bonus in outfit string, we will think it is a legendary part.
---If someone also extends their uses_parts table with other attachments, we might think that they are using a legendary attachment -> handle in NetworkPeer, don't flag if legendary attachment is not on original gun.
-Hooks:PostHook(WeaponFactoryTweakData, "create_bonuses", "sdss_post_WeaponFactoryTweakData_create_bonuses", function(self, ...)
-	if SDSS._settings.sdss_allow_variants then
-		--Midas Touch
-		--Invisible in third person on akimbo for both vanilla and modded peers, as both client and host.
-		table.insert(self.wpn_fps_x_deagle.uses_parts, "wpn_fps_pis_deagle_b_legend")
-		
-		--Santa's Slayers
-		--Invisible in third person on single for both vanilla and modded peers, as both client and host.
-		table.insert(self.wpn_fps_pis_1911.uses_parts, "wpn_fps_pis_1911_g_legendary")
-		table.insert(self.wpn_fps_pis_1911.uses_parts, "wpn_fps_pis_1911_fl_legendary")
-	end
-end)
-
 --Set up legendary parts
 Hooks:PostHook(WeaponFactoryTweakData, "_init_legendary", "sdss_post_WeaponFactoryTweakData__init_legendary", function(self)
 	--Set up dummy parts
 	--Used when "allow variants" is off, this way we can hide the legendary parts from the akimbos in inventory.
-	--Set unatainable to nil to prevent false cheater tags since dummy part isn't in default_blueprint.
-	--Edge case: someone cheats legendary skin, puts it on akimbo variant, and you have disabled variants -> not detected.
+	--Parts are tagged as "unatainable", handle validation in NetworkPeer.
 	self.parts.wpn_fps_smg_p90_b_legend_dummy = deep_clone(self.parts.wpn_fps_smg_p90_b_legend)
-	self.parts.wpn_fps_smg_p90_b_legend_dummy.unatainable = nil
-	
 	self.parts.wpn_fps_pis_judge_b_legend_dummy = deep_clone(self.parts.wpn_fps_pis_judge_b_legend)
-	self.parts.wpn_fps_pis_judge_b_legend_dummy.unatainable = nil
-	
 	self.parts.wpn_fps_pis_judge_g_legend_dummy = deep_clone(self.parts.wpn_fps_pis_judge_g_legend)
-	self.parts.wpn_fps_pis_judge_g_legend_dummy.unatainable = nil
 	
 	--Set up legendary parts
 	local new_values = {
